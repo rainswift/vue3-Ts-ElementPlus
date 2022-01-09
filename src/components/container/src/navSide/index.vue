4da82eed7fd50081eb661dbca84aebc8abb20229
rainswift <837484536@qq.com>
2022-01-09 22:30:54 +0800

    修改导航栏

--- src/components/container/src/navSide/index.vue
+++ src/components/container/src/navSide/index.vue
@@ -1,28 +1,19 @@
 <template>
     <el-menu
-      default-active="2"
+      default-active="1"
       class="el-menu-vertical-demo"
-      :collapse="collapse"
+      :router="true"
     >
-      <el-menu-item index="2">
+      <el-menu-item index="/chooseArea">
         <!-- <el-icon-fold></el-icon-fold> -->
-        <template #title>Navigator Two</template>
-      </el-menu-item>
-      <el-menu-item index="3" disabled>
-        <el-icon><document /></el-icon>
-        <template #title>Navigator Three</template>
-      </el-menu-item>
-      <el-menu-item index="4">
-        <el-icon><setting /></el-icon>
-        <template #title>Navigator Four</template>
+        <template #title>省市区选择</template>
       </el-menu-item>
   </el-menu>
 </template>
 
 <script lang="ts" setup>
-  let props = defineProps<{
-    collapse: boolean
-  }>()
+
+  
 </script>
 
 <style>
