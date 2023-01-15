147047aff3737e5ed33a783dec10c920f1aa90b5
rainswift <837484536@qq.com>
2023-01-16 00:20:23 +0800

    菜单

---
+++ my-vue-ts/src/components/container/src/navSide/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <el-menu
+      default-active="2"
+      class="el-menu-vertical-demo"
+      :collapse="collapse"
+    >
+      <el-menu-item index="2">
+        <!-- <el-icon-fold></el-icon-fold> -->
+        <template #title>Navigator Two</template>
+      </el-menu-item>
+      <el-menu-item index="3" disabled>
+        <el-icon><document /></el-icon>
+        <template #title>Navigator Three</template>
+      </el-menu-item>
+      <el-menu-item index="4">
+        <el-icon><setting /></el-icon>
+        <template #title>Navigator Four</template>
+      </el-menu-item>
+  </el-menu>
+</template>
+
+<script lang="ts" setup>
+  let props = defineProps<{
+    collapse: boolean
+  }>()
+</script>
+
+<style>
+
+</style>
